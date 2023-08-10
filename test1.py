657dcbfff42fa4e31440eec3e7e25645e99cc148
viraj2011 <107635321+viraj2011@users.noreply.github.com>
2023-08-10 18:06:10 +0530

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,14 @@
+a = 33
+b = "33"
+
+if b > a:
+    print("B is grater than A")
+elif a == b:
+    print("A and B are equal")  
+
+def my_function(fname):
+    print(fname + "Refsnes")   
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
